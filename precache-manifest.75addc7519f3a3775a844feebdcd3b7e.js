7419cfd9cbcb811b11d537a3c12dffa6567fd2ce
Megan Hunter <mnhunter@outlook.com>
2020-03-17 11:18:13 +1000

    Updates

---
+++ precache-manifest.75addc7519f3a3775a844feebdcd3b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecc53e3d38d9793a1f364b3932b8217",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a7492e6d1bacc765118d",
+    "url": "/monsters-rolodex/static/css/main.1335e3aa.chunk.css"
+  },
+  {
+    "revision": "3dd738a58da8e2f40ee0",
+    "url": "/monsters-rolodex/static/js/2.2fdbc417.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.2fdbc417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7492e6d1bacc765118d",
+    "url": "/monsters-rolodex/static/js/main.a85b83d5.chunk.js"
+  },
+  {
+    "revision": "22a6d7bf43cbffaaa8d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab4bb4ab.js"
+  }
+]);
